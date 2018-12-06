36e714c23305d0fc2e95dbb9327ea1c914570eb6
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2018-12-06 17:22:30 +0200

    Add a component

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,18 @@
 <div class="full-width center-content">
 <h1>Hello world!</h1>
 <p>Nam!</p>
+<HelloComponent name="Antti" />
 </div>
 </template>
+
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+	components: {
+		HelloComponent
+	}
+}
+</script>
 
 <style lang="stylus" scoped>
 .full-width {
