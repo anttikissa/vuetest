9e121c98b58766f1d25ce6ad38d86a70986f5157
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2018-12-06 17:08:56 +0200

    CSS, Stylus, hot loading

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
-<div>
+<div class="full-width center-content">
 <h1>Hello world!</h1>
-<p>This is text</p>
+<p>Nam!</p>
 </div>
 </template>
+
+<style lang="stylus" scoped>
+.full-width {
+	width: 100%
+}
+
+.center-content {
+	display: flex
+	justify-content: center
+	align-items: center
+}
+
+p {
+	color: blue
+}
+</style>
